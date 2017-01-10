96ed5c160d1579a34b97e9b87bf743bbdec954a7
Alec Muffett <alec.muffett@gmail.com>
2017-01-10 21:20:15 +0000

    commit: do not rsync git repos

--- remote-pull.sh
+++ remote-pull.sh
@@ -7,5 +7,5 @@ cd $RIG_REMOTE_DIR || exit 1
 for host in $RIG_HOSTS
 do
     # assume trailing slash in variables
-    rsync-linktree.sh --delete --delete-excluded --exclude="cached*" $host:$RIG_OUTPUT_DIR tmp-$host/
+    rsync-linktree.sh --delete --delete-excluded --exclude="cached*" --exclude=".git" $host:$RIG_OUTPUT_DIR tmp-$host/
 done
