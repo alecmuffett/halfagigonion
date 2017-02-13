612c977fc04398ba249aa564db39086e90916184
Alec Muffett <alec.muffett@gmail.com>
2017-02-13 09:09:50 +0000

    commit: ls-hsdir

--- ls-hsdir.py
+++ ls-hsdir.py
@@ -1,23 +1,39 @@
 #!/usr/bin/env python
 
-import sys
-import datetime
-
 from stem.control import Controller
+from time import gmtime, strftime
+import datetime
+import stem
+import sys
 
 now = datetime.datetime.utcnow()
+now2 = now.strftime('%Y-%m-%d %H:%M:%S')
 
 def print_hsdir(controller, name):
-    x = controller.get_hidden_service_descriptor(name)
+    try:
+        x = controller.get_hidden_service_descriptor(name)
+    except stem.DescriptorUnavailable:
+        print "# unavailable", name
+        return
+
+    ips = sorted([ "%s:%d" % (ip.address, ip.port) for ip in x.introduction_points()])
     age = now - x.published
-    print "v=%d age=%d pub(%s) %s" % (
+
+    print "v=%d age=%d nip=%d pub(%s) now(%s) %s" % (
         x.version,
         age.total_seconds(),
+        len(ips),
         x.published,
+        now2,
         name
     )
 
-with Controller.from_port(port = 9051) as controller:
+    i = 0
+    for ip in ips:
+        print "\t%d: %s" % (i, ip)
+        i = i + 1
+
+with Controller.from_port(port = 9151) as controller:
     controller.authenticate()
     for name in sys.argv[1:]:
         if name.endswith('.onion'):
